6a93d9503233390764bbc626d7c9643b09ae0dcc
automatix <info@automatix.info>
2018-04-24 23:19:17 +0200

    #5 missing `use` statement added

--- src/Process/EventListenerInterface.php
+++ src/Process/EventListenerInterface.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Process;
 
+use App\Base\Enums\Processes\EventNames\AbstractEventName;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
