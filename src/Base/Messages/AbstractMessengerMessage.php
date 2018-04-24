6002c42b4bc7cb6892e0c228ce3bdda241edf011
automatix <info@automatix.info>
2018-04-24 23:31:09 +0200

    #5 An `AbstractMessengerMessage` added.

---
+++ src/Base/Messages/AbstractMessengerMessage.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Base\Messages;
+
+/**
+ * Class AbstractMessengerMessage
+ *
+ * @package App\Base\Messages
+ * @author Ilya Khanataev <contact@mevatex.com>
+ */
+class AbstractMessengerMessage
+{
+
+
+
+}
