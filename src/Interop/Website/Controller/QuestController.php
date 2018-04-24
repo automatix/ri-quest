f673f0a5a6860377dec9810c727d0cc75438212d
automatix <info@automatix.info>
2018-04-24 23:29:49 +0200

    #5 A `QuestController` added. (Needs to be implemented correctly later.)

---
+++ src/Interop/Website/Controller/QuestController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Interop\Website\Controller;
+
+use App\Services\Dummy\External\FooServiceInterface;
+use App\Services\Process\ProcessHandlingServiceInterface;
+use App\Services\Process\StateManagingServiceInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class QuestController extends Controller
+{
+
+    /** @var StateManagingServiceInterface $stateManagingService */
+    private $stateManagingService;
+    /** @var ProcessHandlingServiceInterface $processHandlingService */
+    private $processHandlingService;
+
+    /**
+     * QuestController constructor.
+     *
+     * @param StateManagingServiceInterface $stateManagingService
+     * @param ProcessHandlingServiceInterface $processHandlingService
+     */
+    public function __construct(
+        StateManagingServiceInterface $stateManagingService,
+        ProcessHandlingServiceInterface $processHandlingService
+    ) {
+        $this->stateManagingService = $stateManagingService;
+        $this->processHandlingService = $processHandlingService;
+    }
+
+    public function processUserMessageAction(Request $request)
+    {
+        $currentState = $this->stateManagingService->detectFullState();
+        $input = $request->get('message');
+        $message = $this->messageMappingService->map($input, new IncomingMessage());
+        $this->processHandlingService->handleState($currentState, $message);
+
+        return new Response();
+    }
+
+}
