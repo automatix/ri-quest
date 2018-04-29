e71de558bf7a93c4ad245e503ae32ba3f7497c69
automatix <info@automatix.info>
2018-04-29 19:46:26 +0200

    #5 The `QuestController` corrected. A `GenericEvent` (extending the Symfony `GenericEvent`) added.

---
+++ src/Base/Enums/Processes/Events/GenericEvent.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Base\Enums\Processes\Events;
+
+use Symfony\Component\EventDispatcher\GenericEvent as SymfonyGenericEvent;
+
+class GenericEvent extends SymfonyGenericEvent
+{
+
+
+
+}
--- src/Interop/Website/Controller/QuestController.php
+++ src/Interop/Website/Controller/QuestController.php
@@ -1,43 +1,45 @@
 <?php
 namespace App\Interop\Website\Controller;
 
+use App\Base\Enums\Processes\EventNames\GeneralEventName;
+use App\Base\Enums\Processes\Events\GenericEvent;
 use App\Process\EventHandlerInterface;
-use App\Services\Dummy\External\FooServiceInterface;
-use App\Services\Process\ProcessHandlingServiceInterface;
 use App\Services\Process\StateManagingServiceInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class QuestController extends Controller
 {
 
     /** @var StateManagingServiceInterface $stateManagingService */
     private $stateManagingService;
-    /** @var ProcessHandlingServiceInterface $processHandlingService */
-    private $processHandlingService;
+    /** @var EventHandlerInterface $eventHandler */
+    private $eventHandler;
+    /** @var EventDispatcherInterface */
+    private $eventDispatcher;
 
     /**
      * QuestController constructor.
      *
      * @param StateManagingServiceInterface $stateManagingService
-     * @param ProcessHandlingServiceInterface $processHandlingService
+     * @param EventHandlerInterface $processHandlingService
      */
     public function __construct(
         StateManagingServiceInterface $stateManagingService,
-        EventHandlerInterface $processHandlingService
+        EventHandlerInterface $processHandlingService,
+        EventDispatcherInterface $eventDispatcher
     ) {
         $this->stateManagingService = $stateManagingService;
-        $this->processHandlingService = $processHandlingService;
+        $this->eventHandler = $processHandlingService;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function processUserMessageAction(Request $request)
     {
-        $currentState = $this->stateManagingService->detectFullState();
-        $input = $request->get('message');
-        $message = $this->messageMappingService->map($input, new IncomingMessage());
-        $this->processHandlingService->handleState($currentState, $message);
+        $message = $request->get('message');
+        $this->eventHandler->handle(new GenericEvent($message), GeneralEventName::USER_MESSAGE_RECEIVED(), $this->eventDispatcher);
 
         return new Response();
     }
