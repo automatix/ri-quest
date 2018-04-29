3dc6253b6ed8b24f634ea5b343b6b9a4ea55b7ef
automatix <info@automatix.info>
2018-04-29 19:47:14 +0200

    #5 naming

--- src/Process/EventHandler.php
+++ src/Process/EventHandler.php
@@ -2,6 +2,7 @@
 namespace App\Process;
 
 use App\Base\Enums\Processes\EventNames\AbstractEventName;
+use App\Process\StateHandlers\StateHandlerInterface;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -9,17 +10,17 @@ class EventHandler implements EventHandlerInterface
 {
 
     /** @var StateHandlerInterface[] */
-    private $handlers;
+    private $stateHandlers;
 
     public function __construct(... $listeners)
     {
-        $this->handlers = $listeners;
+        $this->stateHandlers = $listeners;
     }
 
     public function handle(Event $event, AbstractEventName $eventName, EventDispatcherInterface $eventDispatcher)
     {
         $eventType = AbstractEventName::search($eventName);
-        foreach ($this->handlers as $handler) {
+        foreach ($this->stateHandlers as $handler) {
             $handler->handle($event, $eventType, $eventDispatcher);
         }
     }
