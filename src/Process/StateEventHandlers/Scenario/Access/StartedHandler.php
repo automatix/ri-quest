89e7c00ab5fdb1bb65d9c7fecbe206be51ad1894
automatix <info@automatix.info>
2018-05-11 14:47:34 +0200

    #5 A `StateHandler` added.

---
+++ src/Process/StateEventHandlers/Scenario/Access/StartedHandler.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Process\StateEventHandlers\Scenario\Access;
+
+use App\Process\StateEventHandlers\AbstractStateEventHandler;
+
+/**
+ * @package App\Services\Process\Internal\StateHandlers\Scenario
+ * @author Ilya Khanataev <contact@mevatex.com>
+ */
+class StartedHandler extends AbstractStateEventHandler
+{
+
+}
