3d9b797f65d6639f258487381f3051e2fe6d619c
automatix <info@automatix.info>
2018-05-11 16:24:34 +0200

    #5 The `QuestController` cleaned up: Obsolete code removed.

--- src/Interop/Website/Controller/QuestController.php
+++ src/Interop/Website/Controller/QuestController.php
@@ -3,8 +3,6 @@ namespace App\Interop\Website\Controller;
 
 use App\Base\Enums\Processes\EventNames\EventName;
 use App\Base\Enums\Processes\Events\GenericEvent;
-use App\Process\SystemEventHandlerInterface;
-use App\Services\Process\StateManagingServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,29 +11,23 @@ use Symfony\Component\HttpFoundation\Response;
 class QuestController extends Controller
 {
 
-    /** @var StateManagingServiceInterface $stateManagingService */
-    private $stateManagingService;
-    /** @var SystemEventHandlerInterface $systemEventHandler */
-    private $systemEventHandler;
     /** @var EventDispatcherInterface */
     private $eventDispatcher;
 
     /**
      * QuestController constructor.
      *
-     * @param StateManagingServiceInterface $stateManagingService
-     * @param SystemEventHandlerInterface $systemEventHandler
+     * @param EventDispatcherInterface $eventDispatcher
      */
-    public function __construct(
-        StateManagingServiceInterface $stateManagingService,
-        SystemEventHandlerInterface $systemEventHandler,
-        EventDispatcherInterface $eventDispatcher
-    ) {
-        $this->stateManagingService = $stateManagingService;
-        $this->systemEventHandler = $systemEventHandler;
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
         $this->eventDispatcher = $eventDispatcher;
     }
 
+    /**
+     * @param Request $request
+     * @return Response
+     */
     public function processUserMessageAction(Request $request)
     {
         $message = $request->get('message');
