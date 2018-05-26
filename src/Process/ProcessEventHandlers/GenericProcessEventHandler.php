8c6b38b07dc0f9a4720c2694a0d96a7ef6590c00
automatix <info@automatix.info>
2018-05-26 23:27:18 +0200

    #5 The obsolete `AbstractProcessEventHandler` eliminated.

--- src/Process/ProcessEventHandlers/GenericProcessEventHandler.php
+++ src/Process/ProcessEventHandlers/GenericProcessEventHandler.php
@@ -3,13 +3,41 @@ namespace App\Process\ProcessEventHandlers;
 
 use App\Base\Enums\Processes\EventNames\EventName;
 use App\Base\Enums\Processes\ProcessName;
-use App\Base\Enums\Processes\States\AbstractProcessState;
 use App\Base\Exceptions\EventHandlingException;
+use App\Process\HandlerRegistry\StateEventHandlerRegistryInterface;
+use App\Services\Process\StateManagingServiceInterface;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-class GenericProcessEventHandler extends AbstractProcessEventHandler
+class GenericProcessEventHandler implements ProcessEventHandlerInterface
 {
+
+    /** @var StateManagingServiceInterface $stateManagingService */
+    private $stateManagingService;
+    /** @var StateEventHandlerRegistryInterface $stateEventHandlerRegistry */
+    private $stateEventHandlerRegistry;
+
+    public function __construct(StateManagingServiceInterface $stateManagingService, StateEventHandlerRegistryInterface $stateEventHandlerRegistry)
+    {
+        $this->stateManagingService = $stateManagingService;
+        $this->stateEventHandlerRegistry = $stateEventHandlerRegistry;
+    }
+
+    /**
+     * @return StateManagingServiceInterface
+     */
+    public function getStateManagingService(): StateManagingServiceInterface
+    {
+        return $this->stateManagingService;
+    }
+
+    /**
+     * @return StateEventHandlerRegistryInterface
+     */
+    public function getEventHandlerRegistry(): StateEventHandlerRegistryInterface
+    {
+        return $this->stateEventHandlerRegistry;
+    }
 
     public function handle(ProcessName $processName, Event $event, EventName $eventName, EventDispatcherInterface $eventDispatcher)
     {
