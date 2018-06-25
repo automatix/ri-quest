00f4c224e39621dd7f2d3f7a07928a3884d750a5
automatix <info@automatix.info>
2018-06-25 22:01:24 +0200

    #2 The missing `ConcreteProcess` sub-class `PoiConcreteProcess` added.

---
+++ src/Base/Entity/PoiConcreteProcess.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Base\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PoiConcreteProcess
+ *
+ * @ORM\Entity
+ */
+class PoiConcreteProcess extends ConcreteProcess
+{
+
+}
