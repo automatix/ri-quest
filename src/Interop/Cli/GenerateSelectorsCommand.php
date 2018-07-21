de82db54f461ee9a7766e7c65ce9b36e5cf37eb5
automatix <info@automatix.info>
2018-07-21 22:25:28 +0200

    #8 The `GenerateSelectorsCommand` implemented.

--- src/Interop/Cli/GenerateSelectorsCommand.php
+++ src/Interop/Cli/GenerateSelectorsCommand.php
@@ -1,13 +1,29 @@
 <?php
 namespace App\Interop\Cli;
 
-use App\Kernel;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * The GenerateSelectorsCommand generates ID Selectors for every Entity class
+ * excepting the AbstractEntity.
+ * So every Foo Entity class gets a FooSelector class,
+ * every AbstractFoo class gets a FooSelector class.
+ *
+ * Usege example:
+ * $ cd /path/to/project
+ * $ bin/console app:generate-selectors -s './Base/Entity' -t './Base/Selectors'
+ *
+ * @package App\Interop\Cli
+ * @author Ilya Khanataev <contact@mevatex.com>
+ * @todo Refactor it and write tests!
+ * @todo Write a help!
+ */
 class GenerateSelectorsCommand extends ContainerAwareCommand
 {
 
@@ -30,8 +46,70 @@ class GenerateSelectorsCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $projectRoot = $this->getContainer()->get('kernel')->getRootDir();
-        echo implode(',', [$input->getOption('entities_directory'), $input->getOption('selectors_directory')]) . PHP_EOL;
-        $output->writeln($projectRoot . '~~~' . PHP_EOL);
+        $entitiesDir = $projectRoot . preg_replace('/^\.\//', '/', $input->getOption('entities_directory'));
+        $selectorsDir = $projectRoot . preg_replace('/^\.\//', '/', $input->getOption('selectors_directory'));
+
+        /** @var RecursiveIteratorIterator $iterator */
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($entitiesDir));
+        /** @var SplFileInfo $element */
+        foreach ($iterator as $element) {
+            $currentFileName = $iterator->getFilename();
+            if ($element->isFile() && $currentFileName !== 'AbstractEntity') {
+                $subPath = $iterator->getSubPath();
+                $targetPath = $selectorsDir . DIRECTORY_SEPARATOR . $subPath;
+                if (! is_dir($targetPath)) {
+                    mkdir($targetPath, '0777', true);
+                }
+
+                $selectorPathName = $this->normalizePath($targetPath . DIRECTORY_SEPARATOR . $this->getSelectorFileName($currentFileName));
+                copy($element->getPathname(), $selectorPathName);
+                file_put_contents($selectorPathName, $this->getSelectorClassContent($currentFileName, $subPath));
+            }
+        }
+        $output->writeln('');
+    }
+
+    private function getSelectorClassContent(string $entityFileName, string $subPath)
+    {
+        $selectorSubNamespace = $this->getSelectorSubNamespace($subPath);
+        $selectorUseStatament = '';
+        if (! empty($subPath)) {
+            $selectorSubNamespace = '\\' . $selectorSubNamespace;
+            $selectorUseStatament = PHP_EOL . 'use App\\Base\\Selectors\\AbstractSelector;' . PHP_EOL;
+
+        }
+        $selectorClassName = $this->getSelectorClassName($entityFileName);
+        return <<<CONTENT
+<?php
+namespace App\Base\Selectors{$selectorSubNamespace};
+{$selectorUseStatament}
+class {$selectorClassName} extends AbstractSelector
+{
+
+}
+
+CONTENT;
+
+    }
+
+    private function getSelectorFileName(string $entityFileName)
+    {
+        return $this->getSelectorClassName($entityFileName) . '.php';
+    }
+
+    private function getSelectorClassName(string $entityFileName)
+    {
+        return preg_replace(['/^Abstract/', '/.php$/'], '', $entityFileName) . 'Selector';
+    }
+
+    private function getSelectorSubNamespace(string $subPath)
+    {
+        return preg_replace('/([\/]+)|([\\\\]+)/', '\\', $subPath);
+    }
+
+    private function normalizePath(string $path)
+    {
+        return preg_replace('/([\/]+)|([\\\\]+)/', DIRECTORY_SEPARATOR, $path);
     }
 
 }
