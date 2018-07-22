2cf524ac4027a0dce8ad3a62657468f3015bed07
automatix <info@automatix.info>
2018-07-22 15:26:25 +0200

    #9 The `EventHandler`s refactored and got a base class `AbstractEventHandler`.

--- src/Process/AccessEventHandler.php
+++ src/Process/AccessEventHandler.php
@@ -8,51 +8,10 @@ use App\Services\Process\ProcessManagingServiceInterface;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-class AccessEventHandler implements WorkflowEventHandlerInterface
+class AccessEventHandler extends AbstractEventHandler
 {
 
-    /** @var ProcessManagingServiceInterface $processManagingService */
-    private $processManagingService;
-    /** @var StateEventHandlerRegistryInterface $stateEventHandlerRegistry */
-    private $stateEventHandlerRegistry;
-
-    public function __construct(ProcessManagingServiceInterface $processManagingService, StateEventHandlerRegistryInterface $stateEventHandlerRegistry)
-    {
-        $this->processManagingService = $processManagingService;
-        $this->stateEventHandlerRegistry = $stateEventHandlerRegistry;
-    }
-
-    /**
-     * @return ProcessManagingServiceInterface
-     */
-    public function getProcessManagingService(): ProcessManagingServiceInterface
-    {
-        return $this->processManagingService;
-    }
-
-    /**
-     * @return StateEventHandlerRegistryInterface
-     */
-    public function getEventHandlerRegistry(): StateEventHandlerRegistryInterface
-    {
-        return $this->stateEventHandlerRegistry;
-    }
-
-    public function handle(Event $event, string $eventName, EventDispatcherInterface $eventDispatcher)
-    {
-        foreach ($this->getProcessNames() as $processName) {
-            $currentState = $this->getProcessManagingService()->detectProcessState($processName);
-            try {
-                $concreteHandler = $this->getEventHandlerRegistry()->get($processName, $currentState, $eventName);
-                call_user_func($concreteHandler, $event, $eventName, $eventDispatcher);
-            } catch (EventHandlingException $e) {
-                $breakpoint = null;
-                // do nothing...
-            }
-        }
-    }
-
-    private function getProcessNames()
+    protected function getProcessNames()
     {
         // The array index determines the priority and so the handling order!
         // Means: The the order in the list is actually the priority.
