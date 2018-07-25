9d0fcab87aa47b2000f506012c87167d178d2876
automatix <info@automatix.info>
2018-07-26 00:26:28 +0200

    #9 The needless inheritance for the `EventHandler` eliminated.

--- src/Process/WorkflowEventHandler.php
+++ src/Process/WorkflowEventHandler.php
@@ -8,8 +8,50 @@ use App\Services\Process\ProcessManagingServiceInterface;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-class WorkflowEventHandler extends AbstractEventHandler
+class WorkflowEventHandler implements WorkflowEventHandlerInterface
 {
+
+    /** @var ProcessManagingServiceInterface $processManagingService */
+    private $processManagingService;
+    /** @var StateEventHandlerRegistryInterface $stateEventHandlerRegistry */
+    private $stateEventHandlerRegistry;
+
+    public function __construct(ProcessManagingServiceInterface $processManagingService, StateEventHandlerRegistryInterface $stateEventHandlerRegistry)
+    {
+        $this->processManagingService = $processManagingService;
+        $this->stateEventHandlerRegistry = $stateEventHandlerRegistry;
+    }
+
+    /**
+     * @return ProcessManagingServiceInterface
+     */
+    public function getProcessManagingService(): ProcessManagingServiceInterface
+    {
+        return $this->processManagingService;
+    }
+
+    /**
+     * @return StateEventHandlerRegistryInterface
+     */
+    public function getEventHandlerRegistry(): StateEventHandlerRegistryInterface
+    {
+        return $this->stateEventHandlerRegistry;
+    }
+
+    public function handle(Event $event, string $eventName, EventDispatcherInterface $eventDispatcher)
+    {
+        foreach ($this->getProcessNames() as $processName) {
+            $currentState = $this->getProcessManagingService()->detectProcessState($processName);
+            try {
+                $concreteHandler = $this->getEventHandlerRegistry()->get($processName, $currentState, $eventName);
+                call_user_func($concreteHandler, $event, $eventName, $eventDispatcher);
+                break;
+            } catch (EventHandlingException $e) {
+                $breakpoint = null;
+                // do nothing...
+            }
+        }
+    }
 
     protected function getProcessNames()
     {
