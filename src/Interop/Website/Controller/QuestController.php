a25b84e62f18923845bfb6902ab3ad8834618e03
automatix <info@automatix.info>
2018-07-29 12:46:53 +0200

    #9 The creating a `Chat` and a `WorkflowConcreteProcessService` if they not exist implemented in the `QuestController`. (To be refactored and moved to a more appropriate place!)

--- src/Interop/Website/Controller/QuestController.php
+++ src/Interop/Website/Controller/QuestController.php
@@ -1,8 +1,13 @@
 <?php
 namespace App\Interop\Website\Controller;
 
+use App\Base\Enums\Entities\ChatType;
 use App\Base\Enums\EventNames\GeneralEventName;
 use App\Base\Events\GenericEvent;
+use App\Services\Process\ChatServiceInterface;
+use App\Services\Process\ProcessManagingServiceInterface;
+use App\Services\Process\WorkflowConcreteProcessServiceInterface;
+use App\Services\RuntimeContext\RuntimeContextServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,15 +18,34 @@ class QuestController extends Controller
 
     /** @var EventDispatcherInterface */
     private $eventDispatcher;
+    /** @var RuntimeContextServiceInterface */
+    private $runtimeContextService;
+    /** @var ChatServiceInterface */
+    private $chatService;
+    /** @var ProcessManagingServiceInterface */
+    private $processManagingService;
+    /** @var WorkflowConcreteProcessServiceInterface */
+    private $workflowConcreteProcessService;
 
     /**
      * QuestController constructor.
-     *
      * @param EventDispatcherInterface $eventDispatcher
+     * @param RuntimeContextServiceInterface $runtimeContextService
+     * @param ChatServiceInterface $chatService
+     * @param ProcessManagingServiceInterface $processManagingService
      */
-    public function __construct(EventDispatcherInterface $eventDispatcher)
-    {
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        RuntimeContextServiceInterface $runtimeContextService,
+        ChatServiceInterface $chatService,
+        ProcessManagingServiceInterface $processManagingService,
+        WorkflowConcreteProcessServiceInterface $workflowConcreteProcessService
+    ) {
         $this->eventDispatcher = $eventDispatcher;
+        $this->runtimeContextService = $runtimeContextService;
+        $this->chatService = $chatService;
+        $this->processManagingService = $processManagingService;
+        $this->workflowConcreteProcessService = $workflowConcreteProcessService;
     }
 
     /**
@@ -30,6 +54,18 @@ class QuestController extends Controller
      */
     public function processUserMessageAction(Request $request)
     {
+        // todo It's just a mock. The Chat ID should be fetched from the ChatBotService.
+        $chatId = $request->get('chat_id');
+        $chat = $this->chatService->findOneByIdentifierAndType($chatId, ChatType::TELEGRAM());
+        $chat = $chat
+            ?: $this->chatService->create($chatId, ChatType::TELEGRAM())
+        ;
+        $workflowConcreteProcess = $this->chatService->findActiveWorkflowConcreteProcessForChat($chat);
+        $workflowConcreteProcess = $workflowConcreteProcess
+            ?: $this->workflowConcreteProcessService->create($chat)
+        ;
+        $this->runtimeContextService->initializeRuntimeContext($chat);
+
         $message = $request->get('message');
         $this->eventDispatcher->dispatch(GeneralEventName::USER_MESSAGE_RECEIVED, new GenericEvent($message));
 
